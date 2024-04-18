8cfceb98e6f9cbd2f06de1961a2437de4ca0f9f4
Juan Manuel Cavilla <juanmcavilla@gmail.com>
2024-04-18 13:07:47 -0300

    Register page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginPageModule } from './login/login.module';
+import { RegisterPageModule } from './register/register.module';
 
 const routes: Routes = [
   {
@@ -10,6 +11,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
   {
     path: '',
