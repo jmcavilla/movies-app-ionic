a3d218d6b7329e88d301b9f95d47202172750e3a
Juan Manuel Cavilla <juanmcavilla@gmail.com>
2024-04-18 15:04:28 -0300

    Movies API Rest

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MovieData } from 'src/services/movies';
+import { MoviesService } from 'src/services/movies.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public movies: Array<MovieData> = [];
+  constructor( private moviesService: MoviesService) {
+    this.getMovies()
+  }
 
-  constructor() {}
+  async getMovies(){
+    const resp = await this.moviesService.getMovies()
+    const data = await resp.json();
+    this.movies = data.results;
+
+    console.log(this.movies)
+  }
 
 }
